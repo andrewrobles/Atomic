8f247c9851345a8829476eae32fe223486c10b6d
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-11-24 20:01:41 -0800

    log the contents of the ping endpoint in the ui

--- web/api/index.js
+++ web/api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 // import records from "./routes/record.js";
 import pong from "./routes/pong.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 // app.use(express.json());
 // app.use("/record", records);
 app.use("/ping", pong);
--- web/ui/src/App.js
+++ web/ui/src/App.js
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // Call to an API endpoint
+    fetch('http://localhost:5050/ping')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // Parse JSON response
+      })
+      .then((data) => {
+        console.log('API Response:', data); // Log the response data
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }, []); // Empty dependency array to ensure the effect runs only once on component mount
+
   return (
     <div className="App">
       <header className="App-header">
