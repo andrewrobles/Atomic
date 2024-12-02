87df888518e91c813cfb27009c16907c61224cd2
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-12-01 17:18:51 -0800

    display habits

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,20 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
-const PROD_HOST = 'https://habits-api.netlify.app'
-const LOCAL_HOST = 'http://localhost:8888'
-const BASE_URL = window.location.hostname === 'localhost' ? LOCAL_HOST : PROD_HOST
+const PROD_HOST = 'https://habits-api.netlify.app';
+const LOCAL_HOST = 'http://localhost:8888';
+const BASE_URL = window.location.hostname === 'localhost' ? LOCAL_HOST : PROD_HOST;
 
 function App() {
+  const [habits, setHabits] = useState([]); // State to store the fetched data
+  const [error, setError] = useState(null); // State to handle errors
+
   useEffect(() => {
     // Call to an API endpoint
-    fetch(BASE_URL  + '/ping')
+    fetch(BASE_URL + '/habits')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok ' + response.statusText);
@@ -17,29 +22,25 @@ function App() {
         return response.json(); // Parse JSON response
       })
       .then((data) => {
-        console.log('API Response:', data); // Log the response data
+        setHabits(data); // Update state with the fetched data
       })
       .catch((error) => {
-        console.error('There was a problem with the fetch operation:', error);
+        setError(error.message); // Update state with the error message
       });
   }, []); // Empty dependency array to ensure the effect runs only once on component mount
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {error && <p className="error">Error: {error}</p>}
+      <div style={{ padding: '16px' }}>
+        <List>
+          {habits.map((item, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 }
