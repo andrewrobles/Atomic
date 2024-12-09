16101d9f98d81e7a12a058415f5c8db978b71aeb
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-12-09 12:21:46 -0800

    set up firebase

--- web/src/App.js
+++ web/src/App.js
@@ -1,34 +1,52 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+// Auth service
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-import HabitList from './components/HabitList'
-import getHabits from './api/index'
+import { initializeApp } from "firebase/app";
 
-function App() {
-  const [habits, setHabits] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchHabits = async () => {
-      try {
-        const response = await getHabits();
-        setHabits(response.data); 
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-
-    fetchHabits(); 
-  }, []);
-
-  return (
-    <div>
-      {error && <p className="error">Error: {error}</p>}
-      <div style={{ padding: '16px' }}>
-        <HabitList habits={habits} />
-      </div>
-    </div>
-  );
+const firebaseConfig = {
+    apiKey: "AIzaSyBJbonEVB6grM13ZRYCy8N_xH8ENv1e8G0",
+    authDomain: "habits-e1ad9.firebaseapp.com",
+    projectId: "habits-e1ad9",
+    storageBucket: "habits-e1ad9.firebasestorage.app",
+    messagingSenderId: "91926125884",
+    appId: "1:91926125884:web:d4829ade798a40f5125239",
+    measurementId: "G-W0RNCEGKR9"
 }
 
-export default App;
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider()
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
+
+
+const App = () => {
+  return (
+    <>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
+  )
+}
+
+export default App
--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { initializeApp } from "firebase/app"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyBJbonEVB6grM13ZRYCy8N_xH8ENv1e8G0",
+  authDomain: "habits-e1ad9.firebaseapp.com",
+  projectId: "habits-e1ad9",
+  storageBucket: "habits-e1ad9.firebasestorage.app",
+  messagingSenderId: "91926125884",
+  appId: "1:91926125884:web:d4829ade798a40f5125239",
+  measurementId: "G-W0RNCEGKR9"
+};
+
+const app = initializeApp(firebaseConfig)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
