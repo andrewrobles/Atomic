744b826ea852deb01cbcc72860afbb6cb8234419
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-12-14 17:08:29 -0800

    implement delete habits endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -32,6 +32,22 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.delete("/:id", async (req, res) => {
+    const { password } = req.query;
+
+    if (password !== process.env.PASSWORD) {
+        return res.status(401).json({ error: "Unauthorized: Invalid or missing password" });
+    }
+
+    try {
+        await client.db("habitsdb").collection("habits").deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+        res.status(200).json({ message: "Habit deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting habit:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 main()
     .then(console.log)
     .catch(console.error)
--- delete-habit.md
+++ delete-habit.md
@@ -2,6 +2,7 @@
 - [x] 3 dots on the habit
 - [x] Habit actions pop-up
 - [x] Are you sure pop-up
-- [ ] API endpoint
+- [x] API endpoint
 - [ ] Call API endpoint
+- [ ] If I exit the habit actions pop-up, the habit button says loading for a millisecond
 - [ ] Refactor HabitList.js
